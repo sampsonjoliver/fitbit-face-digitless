ee0b7662112385631ef4a5c3a8ec8cb2e182a4a5
sampsonoliver <sampsonjoliver@outlook.com>
2019-10-08 23:34:28 +1100

    Customise a few things

--- app/index.ts
+++ app/index.ts
@@ -1,119 +1,158 @@
-import clock from "clock";
-import document from "document";
-import {locale, preferences} from "user-settings";
-import {me} from "appbit";
-import {BodyPresenceSensor} from "body-presence";
-import {HeartRateSensor} from "heart-rate";
-import {today} from "user-activity";
-import {display} from "display";
-import {onSettingChange} from "./settings";
-import {getDateInWordsInstance} from "./time";
+import clock from 'clock';
+import document from 'document';
+import { locale, preferences } from 'user-settings';
+import { me } from 'appbit';
+import { BodyPresenceSensor } from 'body-presence';
+import { HeartRateSensor } from 'heart-rate';
+import { today } from 'user-activity';
+import { display } from 'display';
+import { battery, charger } from 'power';
+import { onSettingChange } from './settings';
+import { getDateInWordsInstance } from './time';
 
 // Update the clock every minute
-clock.granularity = "seconds";
+clock.granularity = 'seconds';
 
 // Get a handle on the <text> element
-const rootElement = document.getElementById("root") as ContainerElement;
-const backgroundElement = document.getElementById("background") as ContainerElement;
-const hoursElement = document.getElementById("hours") as TextElement;
-const minsElement = document.getElementById("minutes") as TextElement;
-const ampmElement = document.getElementById("ampm") as TextElement;
-const dayElement = document.getElementById("day") as TextElement;
-const dateElement = document.getElementById("date") as TextElement;
-const sepElement = document.getElementById("sep") as LineElement;
-const hrElement = document.getElementById("hr") as TextElement;
-const stepsElement = document.getElementById("steps") as TextElement;
-const calsElement = document.getElementById("cals") as TextElement;
+const rootElement = document.getElementById('root') as ContainerElement;
+const backgroundElement = document.getElementById(
+  'background'
+) as ContainerElement;
+const hoursElement = document.getElementById('hours') as TextElement;
+const minsElement = document.getElementById('minutes') as TextElement;
+const ampmElement = document.getElementById('ampm') as TextElement;
+const dayElement = document.getElementById('day') as TextElement;
+const dateElement = document.getElementById('date') as TextElement;
+const sepElement = document.getElementById('sep') as LineElement;
+const batteryIndicatorElement = document.getElementById(
+  'batteryIndicator'
+) as LineElement;
+const hrElement = document.getElementById('hr') as TextElement;
+const stepsElement = document.getElementById('steps') as TextElement;
+const calsElement = document.getElementById('cals') as TextElement;
 
-const backgroundElements = document.getElementsByClassName("background");
-const coloredElements = document.getElementsByClassName("colored");
+const backgroundElements = document.getElementsByClassName('background');
+const coloredElements = document.getElementsByClassName('colored');
 
-const hiddenElements = document.getElementsByClassName("hide");
+const hiddenElements = document.getElementsByClassName('hide');
 
 let enableNeat = true;
 let lastDate: Date;
 let foregroundColor = 'fb-aqua';
 
-function updateSecondHand(date: Date) {
-    let middle = rootElement.width / 2;
-    let secondHand = .90 * middle;
-    if (clock.granularity === "seconds") {
-        secondHand = Math.floor(date.getSeconds() * secondHand / 60);
-    }
-    sepElement.x1 = middle - secondHand;
-    sepElement.x2 = middle + secondHand;
-}
-
 function toggleDisplay(on: boolean) {
-    hiddenElements.forEach(value => {
-        ((value as unknown) as Styled).style.opacity = on ? 1 : 0;
-    });
-    coloredElements.forEach(value => {
-        ((value as unknown) as Styled).style.fill = on ? foregroundColor : 'white';
-    })
+  hiddenElements.forEach(value => {
+    ((value as unknown) as Styled).style.opacity = on ? 1 : 0;
+  });
+  coloredElements.forEach(value => {
+    ((value as unknown) as Styled).style.fill = on ? foregroundColor : 'white';
+  });
 }
 
 // Update the <text> element every tick with the current time
-clock.ontick = (evt) => {
-    let dateInWords = getDateInWordsInstance(preferences.clockDisplay === "12h", evt.date, locale.language);
-    hoursElement.text = dateInWords.formatHours();
-    minsElement.text = dateInWords.formatMinutes();
-    ampmElement.text = dateInWords.formatAmPm();
-    dayElement.text = dateInWords.formatWeekday();
-    dateElement.text = dateInWords.formatDate();
-    stepsElement.text = today.adjusted.steps ? `${today.adjusted.steps}` : "-";
-    calsElement.text = today.adjusted.calories ? `${today.adjusted.calories}` : "-";
-    lastDate = evt.date;
+clock.ontick = evt => {
+  let dateInWords = getDateInWordsInstance(
+    preferences.clockDisplay === '12h',
+    evt.date,
+    locale.language
+  );
+  hoursElement.text = dateInWords.formatHours();
+  minsElement.text = dateInWords.formatMinutes();
+  ampmElement.text = dateInWords.formatAmPm();
+  dayElement.text = dateInWords.formatWeekday();
+  dateElement.text = dateInWords.formatDate();
+  stepsElement.text = today.adjusted.steps ? `${today.adjusted.steps}` : '-';
+  calsElement.text = today.adjusted.calories
+    ? `${today.adjusted.calories}`
+    : '-';
+  lastDate = evt.date;
+};
 
-    updateSecondHand(lastDate);
+function lerpColor(a: string, b: string, amount: number) {
+  const ah = parseInt(a.replace(/#/g, ''), 16);
+  const ar = ah >> 16,
+    ag = (ah >> 8) & 0xff,
+    ab = ah & 0xff;
+  const bh = parseInt(b.replace(/#/g, ''), 16);
+  const br = bh >> 16,
+    bg = (bh >> 8) & 0xff,
+    bb = bh & 0xff;
+  const rr = ar + amount * (br - ar);
+  const rg = ag + amount * (bg - ag);
+  const rb = ab + amount * (bb - ab);
+
+  return (
+    '#' + (((1 << 24) + (rr << 16) + (rg << 8) + rb) | 0).toString(16).slice(1)
+  );
+}
+
+function updateSecondHand(date: Date) {
+  let middle = rootElement.width / 2;
+  let secondHand = 0.9 * middle;
+  if (clock.granularity === 'seconds') {
+    secondHand = Math.floor((date.getSeconds() * secondHand) / 60);
+  }
+  sepElement.x1 = middle - secondHand;
+  sepElement.x2 = middle + secondHand;
+}
+
+battery.onchange = event => {
+  const chargePercent = battery.chargeLevel / 100;
+  batteryIndicatorElement.x2 = rootElement.width * chargePercent;
+  batteryIndicatorElement.style.fill = lerpColor(
+    '#d30000',
+    '#3bb143',
+    chargePercent
+  );
+  batteryIndicatorElement.style.opacity = 0.5;
 };
 
 let body: BodyPresenceSensor;
 let hrm: HeartRateSensor;
 
-if (me.permissions.granted("access_heart_rate")) {
-    hrm = new HeartRateSensor({frequency: 3});
-    hrm.onreading = () => {
-        hrElement.text = `${hrm.heartRate}`;
-        hrm.timestamp
-    };
+if (me.permissions.granted('access_heart_rate')) {
+  hrm = new HeartRateSensor({ frequency: 3 });
+  hrm.onreading = () => {
+    hrElement.text = `${hrm.heartRate}`;
+    hrm.timestamp;
+  };
 }
-if (me.permissions.granted("access_activity")) {
-    body = new BodyPresenceSensor();
-    body.onreading = () => {
-        if (!body.present) {
-            hrm.stop();
-            hrElement.text = "-";
-            return;
-        }
-        hrm.start();
-    };
-    body.start();
+if (me.permissions.granted('access_activity')) {
+  body = new BodyPresenceSensor();
+  body.onreading = () => {
+    if (!body.present) {
+      hrm.stop();
+      hrElement.text = '-';
+      return;
+    }
+    hrm.start();
+  };
+  body.start();
 }
 
 onSettingChange(s => {
-    backgroundElements.forEach(value => {
-        ((value as unknown) as Styled).style.fill = s.bgColor;
-    });
-    foregroundColor = s.fgColor;
-    coloredElements.forEach(value => {
-        ((value as unknown) as Styled).style.fill = s.fgColor;
-    });
-    clock.granularity = s.disableSeconds ? "minutes" : "seconds";
-    if (s.disableSeconds) {
-        updateSecondHand(lastDate);
-    }
-    ampmElement.style.opacity = s.disableMeridiem ? 0 : 1;
-    enableNeat = !s.disableNeat;
-    toggleDisplay(s.disableNeat);
+  backgroundElements.forEach(value => {
+    ((value as unknown) as Styled).style.fill = s.bgColor;
+  });
+  foregroundColor = s.fgColor;
+  coloredElements.forEach(value => {
+    ((value as unknown) as Styled).style.fill = s.fgColor;
+  });
+  clock.granularity = s.disableSeconds ? 'minutes' : 'seconds';
+  if (s.disableSeconds) {
+    updateSecondHand(lastDate);
+  }
+  ampmElement.style.opacity = s.disableMeridiem ? 0 : 1;
+  enableNeat = !s.disableNeat;
+  toggleDisplay(s.disableNeat);
 });
 
 display.onchange = () => {
-    enableNeat && toggleDisplay(false);
+  enableNeat && toggleDisplay(false);
 };
 
 backgroundElement.onmouseup = () => {
-    toggleDisplay(true);
+  toggleDisplay(true);
 };
 
 toggleDisplay(true);
--- app/time.ts
+++ app/time.ts
@@ -1,140 +1,213 @@
 const onesTable: { [k: string]: string[] } = {
-    'en': ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
-    'fr': ['', 'un', 'deux', 'trois', 'quatre', 'cinq', 'six', 'sept', 'huit', 'neuf'],
-    'it': ['', 'uno', 'due', 'tre', 'quattro', 'cinque', 'sei', 'sette', 'otto', 'nove'],
+  en: [
+    '',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine'
+  ],
+  fr: [
+    '',
+    'un',
+    'deux',
+    'trois',
+    'quatre',
+    'cinq',
+    'six',
+    'sept',
+    'huit',
+    'neuf'
+  ],
+  it: [
+    '',
+    'uno',
+    'due',
+    'tre',
+    'quattro',
+    'cinque',
+    'sei',
+    'sette',
+    'otto',
+    'nove'
+  ]
 };
 const teensTable: { [k: string]: string[] } = {
-    'en': ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
-    'fr': ['dix', 'onze', 'douze', 'treize', 'quatorze', 'quinze', 'seize', 'dix-sept', 'dix-huit', 'dix-neuf'],
-    'it': ['dieci', 'undici', 'dodici', 'tredici', 'quattordici', 'quindici', 'sedici', 'diciassette', 'diciotto', 'diciannove'],
+  en: [
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen'
+  ],
+  fr: [
+    'dix',
+    'onze',
+    'douze',
+    'treize',
+    'quatorze',
+    'quinze',
+    'seize',
+    'dix-sept',
+    'dix-huit',
+    'dix-neuf'
+  ],
+  it: [
+    'dieci',
+    'undici',
+    'dodici',
+    'tredici',
+    'quattordici',
+    'quindici',
+    'sedici',
+    'diciassette',
+    'diciotto',
+    'diciannove'
+  ]
 };
 const tensTable: { [k: string]: string[] } = {
-    'en': ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty'],
-    'fr': ['', 'dix', 'vingt', 'trente', 'quarante', 'cinquante'],
-    'it': ['', 'dieci', 'venti', 'trenta', 'quaranta', 'cinquanta'],
+  en: ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty'],
+  fr: ['', 'dix', 'vingt', 'trente', 'quarante', 'cinquante'],
+  it: ['', 'dieci', 'venti', 'trenta', 'quaranta', 'cinquanta']
 };
 const weekdaysTable: { [k: string]: string[] } = {
-    'en': ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'],
-    'fr': ['dim', 'lun', 'mar', 'mer', 'jeu', 'ven', 'sam'],
-    'it': ['dom', 'lun', 'mar', 'mer', 'gio', 'ven', 'sab'],
+  en: ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'],
+  fr: ['dim', 'lun', 'mar', 'mer', 'jeu', 'ven', 'sam'],
+  it: ['dom', 'lun', 'mar', 'mer', 'gio', 'ven', 'sab']
 };
 const midnightTable: { [k: string]: string } = {
-    'en': 'midnight',
-    'fr': 'minuit',
-    'it': 'mezzanotte',
+  en: 'midnight',
+  fr: 'minuit',
+  it: 'mezzanotte'
 };
 const supportedLanguages: { [k: string]: boolean } = {
-    'en': true,
-    'fr': true,
-    'it': true,
+  en: true,
+  fr: true,
+  it: true
 };
 const defaultLanguage: string = 'en';
 
-export function getDateInWordsInstance(is12h: boolean, date: Date, locale: string) {
-    let language = locale.split('-')[0];
-    if (!supportedLanguages[language]) {
-        language = defaultLanguage;
-    }
-    if (is12h) {
-        return new DateTimeInWords12h(date, language);
-    }
-    return new DateTimeInWords24h(date, language);
+export function getDateInWordsInstance(
+  is12h: boolean,
+  date: Date,
+  locale: string
+) {
+  let language = locale.split('-')[0];
+  if (!supportedLanguages[language]) {
+    language = defaultLanguage;
+  }
+  if (is12h) {
+    return new DateTimeInWords12h(date, language);
+  }
+  return new DateTimeInWords24h(date, language);
 }
 
 export class DateTimeInWords12h {
-    protected pm: boolean;
-    protected date: Date;
-    protected language: string;
+  protected pm: boolean;
+  protected date: Date;
+  protected language: string;
 
-    constructor(date: Date, language: string) {
-        this.date = date;
-        this.pm = date.getHours() >= 12;
-        this.language = language;
-    }
+  constructor(date: Date, language: string) {
+    this.date = date;
+    this.pm = date.getHours() >= 12;
+    this.language = language;
+  }
 
-    protected formatNumber(n: number): string {
-        let units = n % 10;
-        let tens = Math.floor(n / 10);
-        if (units === 0) {
-            return tensTable[this.language][tens];
-        }
-        switch (tens) {
-            case 0:
-                return onesTable[this.language][units];
-            case 1:
-                return teensTable[this.language][units];
-        }
-        if (this.language === 'fr' && units === 1) {
-            return `${tensTable[this.language][tens]} et ${onesTable[this.language][units]}`;
-        }
-        if (this.language === 'it') {
-            if (units === 3) {
-                return `${tensTable[this.language][tens]}tré`;
-            }
-            if (units === 1 || units === 8) {
-                return `${tensTable[this.language][tens].slice(0, -1)}${onesTable[this.language][units]}`;
-            }
-            return `${tensTable[this.language][tens]}${onesTable[this.language][units]}`;
-        }
-        return `${tensTable[this.language][tens]} ${onesTable[this.language][units]}`;
+  protected formatNumber(n: number, onesPrefix?: string): string {
+    let units = n % 10;
+    let tens = Math.floor(n / 10);
+    if (units === 0) {
+      return `${tensTable[this.language][tens]}`;
     }
+    switch (tens) {
+      case 0:
+        return `${onesPrefix ? onesPrefix + ' ' : ''}${
+          onesTable[this.language][units]
+        }`;
+      case 1:
+        return teensTable[this.language][units];
+    }
+    if (this.language === 'fr' && units === 1) {
+      return `${tensTable[this.language][tens]} et ${onesTable[this.language][units]}`;
+    }
+    if (this.language === 'it') {
+      if (units === 3) {
+        return `${tensTable[this.language][tens]}tré`;
+      }
+      if (units === 1 || units === 8) {
+        return `${tensTable[this.language][tens].slice(0, -1)}${
+          onesTable[this.language][units]
+        }`;
+      }
+      return `${tensTable[this.language][tens]}${onesTable[this.language][units]}`;
+    }
+    return `${tensTable[this.language][tens]} ${onesTable[this.language][units]}`;
+  }
 
-    formatHours(): string {
-        return this.formatNumber(this.date.getHours() % 12 || 12);
-    }
+  formatHours(): string {
+    return this.formatNumber(this.date.getHours() % 12 || 12);
+  }
 
-    formatMinutes(): string {
-        if (this.date.getMinutes() === 0) {
-            return " ";
-        }
-        return this.formatNumber(this.date.getMinutes());
+  formatMinutes(): string {
+    if (this.date.getMinutes() === 0) {
+      return "o'clock";
     }
+    return this.formatNumber(this.date.getMinutes(), 'zero');
+  }
 
-    formatAmPm(): string {
-        if (this.pm) {
-            return "pm";
-        }
-        return "am";
+  formatAmPm(): string {
+    if (this.pm) {
+      return 'pm';
     }
+    return 'am';
+  }
 
-    formatWeekday(): string {
-        return weekdaysTable[this.language][this.date.getDay()];
-    }
+  formatWeekday(): string {
+    return weekdaysTable[this.language][this.date.getDay()];
+  }
 
-    formatDate(): string {
-        switch (this.language) {
-            case 'fr':
-            case 'it':
-                return `${zeroPad(this.date.getDate())}/${zeroPad(this.date.getMonth() + 1)}`;
-        }
-        return `${this.date.getMonth() + 1}.${this.date.getDate()}`;
-    }
+  formatDate(): string {
+    return `${zeroPad(this.date.getDate())}.${zeroPad(
+      this.date.getMonth() + 1
+    )}`;
+  }
 }
 
 export class DateTimeInWords24h extends DateTimeInWords12h {
-    constructor(date: Date, language: string) {
-        super(date, language);
-    }
+  constructor(date: Date, language: string) {
+    super(date, language);
+  }
 
-    formatHours(): string {
-        return this.formatNumber(this.date.getHours()) || midnightTable[this.language];
-    }
+  formatHours(): string {
+    return (
+      this.formatNumber(this.date.getHours()) || midnightTable[this.language]
+    );
+  }
 
-    formatMinutes(): string {
-        if (this.date.getMinutes() === 0) {
-            return " ";
-        }
-        return super.formatMinutes() + ".";
+  formatMinutes(): string {
+    if (this.date.getMinutes() === 0) {
+      return "o'clock";
     }
+    return super.formatMinutes() + '.';
+  }
 
-    formatAmPm(): string {
-        return "";
-    }
+  formatAmPm(): string {
+    return '';
+  }
 }
 
 function zeroPad(i: number): string {
-    if (i < 10) {
-        return `0${i}`
-    }
-    return `${i}`;
+  if (i < 10) {
+    return `0${i}`;
+  }
+  return `${i}`;
 }
